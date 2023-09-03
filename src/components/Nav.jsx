7e52569be1e23fc89099563047652ffe95ab4b6e
Rajiv S <rajiv620019@gmail.com>
2023-09-03 13:04:52 +0530

    Nav bar completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,22 @@ const Nav = () => {
         <a href="/">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex-justify-center items-center gap-16 max-lg:hidden" />
+        <ul className="flex-1 flex-justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="hidden max-lg:hidden">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
