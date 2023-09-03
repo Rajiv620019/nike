b444ccf10eb13de4279fad5303f6885687f47359
Rajiv S <rajiv620019@gmail.com>
2023-09-03 19:57:50 +0530

    shoe thumbnail

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,11 +7,21 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
 
   return (
     <div
-      className={`boeder-2 rounded-xl ${
+      className={`border-2 rounded-xl ${
         bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"
       }  cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
-    ></div>
+    >
+      <div className="flex justify-center items-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe thumbnail"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
   );
 };
 export default ShoeCard;
